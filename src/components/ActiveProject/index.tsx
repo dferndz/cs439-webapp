c6a4b58d62cbe349515942c49aa9d547abe9dab7
Daniel Fernandez <39488892+dferndz@users.noreply.github.com>
2021-12-16 02:30:59 -0600

    fix active project

--- src/components/ActiveProject/index.tsx
+++ src/components/ActiveProject/index.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from "react";
-import { CardHeader, Box, Button, ButtonGroup } from "@mui/material";
+import { CardHeader, Box, Card, CardContent, Grid, Link } from "@mui/material";
 import CircleIcon from "@mui/icons-material/Circle";
+import LinkIcon from "@mui/icons-material/Link";
 
 import { observer } from "mobx-react";
 import { useStores } from "../../stores";
@@ -12,34 +13,43 @@ type ActiveProjectProp = {
 
 const ActiveProjectItem = ({ project }: ActiveProjectProp) => {
   const link = `https://www.cs.utexas.edu/~gheith/${project.info_site_name}.html`;
-  const handleClick = useCallback(() => {
-    window.open(link, "_blank");
-  }, [link]);
-  return (
-    <ButtonGroup size="small" variant="outlined" color="secondary">
-      <Button>
-        <Box pt={1} pr={1}>
-          <CircleIcon fontSize="small" color="success" />
-        </Box>
 
-        {project.name}
-      </Button>
-      {project.description && <Button>{project.description}</Button>}
-      <Button onClick={handleClick} color="secondary">
-        Go to website
-      </Button>
-    </ButtonGroup>
+  return (
+    <Card>
+      <CardContent>
+        <Grid container>
+          <Grid item xs={1}>
+            <CircleIcon style={{ fontSize: 12 }} color="success" />
+          </Grid>
+          <Grid item>{project.name}</Grid>
+          <Grid item>
+            <Box pl={2}>
+              <Link href={link} target="_blank" underline="hover">
+                Go to website <LinkIcon style={{ fontSize: 15 }} />
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box pt={2}>{project.description}</Box>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
 const ActiveProject = observer(() => {
   const { projectStore } = useStores();
 
+  if (!projectStore.activeProjects.length) return null;
+
   return (
     <React.Fragment>
       <CardHeader title="Active project" />
       {projectStore.activeProjects.map((project: Project, index: number) => (
-        <ActiveProjectItem key={index} project={project} />
+        <Box key={index} pb={2}>
+          <ActiveProjectItem project={project} />
+        </Box>
       ))}
     </React.Fragment>
   );
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,43 +14,52 @@ const projects: Project[] = [
   {
     name: "P2",
     info_site_name: "cs439_f21_p2",
+    description: "Cooperative multithreading",
   },
   {
     name: "P3",
     info_site_name: "cs439_f21_p3",
+    description: "Premptive multithreading",
   },
   {
     name: "P4",
     info_site_name: "cs439_f21_p4",
+    description: "Synchronization primitives",
   },
   {
     name: "P5",
     info_site_name: "cs439_f21_p5",
+    description: "File systems",
   },
   {
     name: "P6",
     info_site_name: "cs439_f21_p6",
+    description: "Cache",
   },
   {
     name: "P7",
     info_site_name: "cs439_f21_p7",
+    description: "Virtual memory",
   },
   {
     name: "P8",
     info_site_name: "cs439_f21_p8",
+    description: "System calls",
   },
   {
     name: "P9",
     info_site_name: "cs439_f21_p9",
+    description: "Fork and exec",
+    active: true,
   },
   {
     name: "PA",
     info_site_name: "cs439_f21_pa",
+    description: "Final project",
   },
   {
     name: "PB",
     info_site_name: "cs439_f21_pb",
-    active: true,
     description: "Final project",
   },
 ];
